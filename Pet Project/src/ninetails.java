002892a43c95288d07cb610f07becbf642e232e7
thisisbogus <aidenhmorris09@gmail.com>
2025-12-04 09:39:42 -0800

    implemented interface into ninetails class

---
+++ Pet Project/src/ninetails.java
@@ -0,0 +1,22 @@
+public class ninetails implements Pokemon{
+    public void sleep()
+    {
+        
+    }
+    public void train()
+    {
+
+    }
+    public void play()
+    {
+
+    }
+    public void feed()
+    {
+
+    }
+    public void battle()
+    {
+
+    }
+}
