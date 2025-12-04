63fd75146cd3ecbffa9fb0ba04d3a5ead85a2d74
MaxG561 <trainmaster03@yahoo.com>
2025-12-04 09:43:56 -0800

    update

--- Pet Project/src/charizard.java
+++ Pet Project/src/charizard.java
@@ -1,3 +1,48 @@
-public class charizard {
+public class Charizard {
+private  int health;
+private  int energy;
+private  int level;
+private  int temperature;
+
+    public Charizard() {
+        this.energy = 100;
+        this.health = 100;
+        this.level = 1;
+        this.temperature = 36;
+}
+
+
+
+public int getHealth() {
+
+    return health;
+
+}
+
+
+
+public int getEnergy() {
+
+    return energy;
+
+}
+
+
+
+public int getLevel() {
+
+    return level;
+
+}
+
+
+
+public double getTemperature() {
+
+    return temperature;
+
+}
+
+    
 
 }
