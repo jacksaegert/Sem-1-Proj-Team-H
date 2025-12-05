0fa5345b46afcda48f72455c2ae5ccc5a08ca520
jacksaegert <jack.saegert@gmail.com>
2025-12-05 09:23:06 -0800

    Work!!

--- Pet Project/src/App.java
+++ Pet Project/src/App.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to Pokemon Simulator");
+        System.out.println("The One-Stop-Shop fo all Your Pokemon Training Needs!");
+        System.out.println("Press Enter to Begin");
+        input.nextLine();
+        System.out.println("Choose your Pokemon:");
+        System.out.println("\n 1.) Charizard: Manage temperature! \n 2.) Bellsprout: Manage sunlight! \n 3.) Ninetails: Make sure they're well goroomed! \n 4.) Ditto: Swaps to a random pokemon occasionally. Manage everything!");
+
+
+
+
     }
 }
