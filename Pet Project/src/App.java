a7a4e38219fe72fa9b16f1cf311641bfbf1e81b0
jacksaegert <jack.saegert@gmail.com>
2025-12-05 09:38:59 -0800

    Added Pokemon Choice Selector Thingy

--- Pet Project/src/App.java
+++ Pet Project/src/App.java
@@ -7,7 +7,18 @@ public class App {
         System.out.println("Press Enter to Begin");
         input.nextLine();
         System.out.println("Choose your Pokemon:");
-        System.out.println("\n 1.) Charizard: Manage temperature! \n 2.) Bellsprout: Manage sunlight! \n 3.) Ninetails: Make sure they're well goroomed! \n 4.) Ditto: Swaps to a random pokemon occasionally. Manage everything!");
+        System.out.println("\n 1.) Charizard: Manage temperature! \n 2.) Bellsprout: Manage sunlight! \n 3.) Ninetails: Make sure they're well goroomed! \n 4.) Ditto: Swaps to a random pokemon occasionally. Manage everything! \n");
+        int choice = input.nextInt();
+        if (choice == 1){
+            //Charizard Constructor
+        } else if (choice == 2) {
+            //Bellsprout Constructor
+        } else if (choice == 3) {
+            //Ninetails Constructor
+        } else {
+            //Ditto Constructor
+        }
+
 
 
 
