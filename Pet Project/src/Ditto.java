871d83f14954e53284a56412a4f9d54eb7115542
jacksaegert <jack.saegert@gmail.com>
2025-12-08 10:10:28 -0800

    Work!!

--- Pet Project/src/Ditto.java
+++ Pet Project/src/Ditto.java
@@ -1,22 +1,22 @@
-public class Ditto implements Pokemon{
+public class Ditto implements Pokemon {
+    public ditto(){
+        public void sleep(){
+            this.energy += 20;
+            this.mood += 5;
+        }
+        public void train(){
+            this.xp += 15;
+            this.energy -=10;
 
-    public void sleep(){
-        
+        }
+        public void play(){
+
+        }
+        public void feed(){
+
+        }
+        public void battle(){
+
+        }
     }
-    public void train(){
-
-    }
-    public void play(){
-
-    }
-    public void feed(){
-
-    }
-    public void battle(){
-
-    }
-    public void lvlup(){
-
-    }
-
 }
