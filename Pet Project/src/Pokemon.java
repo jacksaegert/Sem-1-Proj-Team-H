e875c8daa7815a98997d66ee7969c9db84d1c549
jacksaegert <jack.saegert@gmail.com>
2025-12-09 09:41:44 -0800

    Added Accessor and Mutator methods to the Interface

--- Pet Project/src/Pokemon.java
+++ Pet Project/src/Pokemon.java
@@ -1,8 +1,15 @@
 interface Pokemon {
     //Blorp
+    public double getXp();
+    public int getHealth();
+    public int getEnergy();
+    public int getLevel();
+    public int getTemperature();
+    public int getMood();
     public void sleep();
     public void train();
     public void play();
     public void feed();
     public void battle();
+    public void lvlup();
 }
